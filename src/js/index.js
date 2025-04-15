bacbcfee37c6f1d45a949ec428d27688d7e2dbea
André Chaibe <andrechaibedev@gmail.com>
2025-04-15 11:58:27 -0300

    Refatoração  na linha do evento no JS e adição de tratamento de erro.

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,23 @@ const botaoGerarConselho = document.querySelector(".botao-atualizar-conselho");
 const identificadorConselho = document.querySelector(".id-conselho");
 const descricaoConselho = document.querySelector(".conselho-descricao");
 
-botaoGerarConselho.addEventListener("click", () => {
-    GerarConselho()
-})
+botaoGerarConselho.addEventListener("click", GerarConselho);
 
-async function  GerarConselho() {
-    const resposta = await fetch("https://api.adviceslip.com/advice");
-    const converterResposta = await resposta.json();
-    identificadorConselho.textContent = converterResposta.slip.id;
-    descricaoConselho.textContent = converterResposta.slip.advice;
+async function GerarConselho() {
+
+    try {
+        const resposta = await fetch("https://api.adviceslip.com/advice");
+
+        if (!resposta.ok) {
+            throw new Error("Ocorreu um erro ao tentar buscar as informações da API.")
+        };
+
+        const converterResposta = await resposta.json();
+        identificadorConselho.textContent = `Conselho número: ${converterResposta.slip.id}`;
+        descricaoConselho.textContent = converterResposta.slip.advice;
+    } catch (error) {
+        identificadorConselho.textContent = "ERROR";
+        descricaoConselho.textContent = error;
+    };
+
 };
